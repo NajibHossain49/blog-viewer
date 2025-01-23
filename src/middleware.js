97845dac84d7e9cf36e8ba5e32f5b15a5ce8de0a
NajibHossain49 <najibhossain1234@gmail.com>
2025-01-23 11:48:32 +0600

    Updated middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,41 @@
+// import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+// import { NextResponse } from "next/server";
+
+// export default withAuth(
+//   function middleware(req) {
+//     return NextResponse.next();
+//   },
+//   {
+//     // Add the routes to protect
+//     callbacks: {
+//       authorized: ({ req }) => {
+//         return true;
+//       },
+//     },
+//   }
+// );
+
+// export const config = {
+//   matcher: ["/profile"],
+// };
+
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
+    const isAuthenticated = req.cookies.get('kinde_token') != null;
+    
+    if (!isAuthenticated) {
+      return NextResponse.redirect(new URL('/api/auth/login', req.url));
+    }
+
     return NextResponse.next();
   },
   {
-    // Add the routes to protect
     callbacks: {
       authorized: ({ req }) => {
-        return true;
+        return req.cookies.get('kinde_token') != null;
       },
     },
   }
