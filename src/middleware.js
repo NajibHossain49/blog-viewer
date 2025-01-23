33711ec634142e1ae1fa81122300f83cf07bc8cc
NajibHossain49 <najibhossain1234@gmail.com>
2025-01-23 13:03:37 +0600

    middleware update

--- src/middleware.js
+++ src/middleware.js
@@ -1,48 +1,15 @@
-import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
-import { NextResponse } from "next/server";
-
-export default withAuth(
-  function middleware(req) {
-    // return NextResponse.next();
-    const res=NextResponse.next();
-    // Add CORS headers
-    res.headers.set("Access-Control-Allow-Origin", "https://blog-viewer-livid.vercel.app");
-    res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-    return res;
-  },
-  {
-    // Add the routes to protect
-    callbacks: {
-      authorized: ({ req }) => {
-        return true;
-      },
-    },
-  }
-);
-
-export const config = {
-  matcher: ["/profile"],
-};
-
 // import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 // import { NextResponse } from "next/server";
 
 // export default withAuth(
 //   function middleware(req) {
-//     const isAuthenticated = req.cookies.get('kinde_token') != null;
-    
-//     if (!isAuthenticated) {
-//       return NextResponse.redirect(new URL('/api/auth/login', req.url));
-//     }
-
 //     return NextResponse.next();
 //   },
 //   {
+//     // Add the routes to protect
 //     callbacks: {
 //       authorized: ({ req }) => {
-//         return req.cookies.get('kinde_token') != null;
+//         return true;
 //       },
 //     },
 //   }
@@ -51,3 +18,29 @@ export const config = {
 // export const config = {
 //   matcher: ["/profile"],
 // };
+
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    const isAuthenticated = req.cookies.get('kinde_token') != null;
+    
+    if (!isAuthenticated) {
+      return NextResponse.redirect(new URL('/api/auth/login', req.url));
+    }
+
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ req }) => {
+        return req.cookies.get('kinde_token') != null;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: ["/profile"],
+};
